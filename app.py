2dec9955a2d655c909aaa1d8ab2bde80b04735df
bhupnderrawat0-eng <bhupnderrawat0@gmail.com>
2026-05-09 22:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
-
+import hashlib
 # =====================================================
 # PAGE CONFIG
 # =====================================================
