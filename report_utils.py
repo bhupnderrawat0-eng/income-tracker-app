ee52f7eba62b9c5bc769239b37aea582601c3708
bhupnderrawat0-eng <bhupnderrawat0@gmail.com>
2026-06-21 22:05:36 +0530

    Update report_utils.py

--- report_utils.py
+++ report_utils.py
@@ -177,8 +177,8 @@ def generate_pdf_report(
 
     try:
         logo = Image("logo.png")
-        logo.drawHeight = 240
-        logo.drawWidth = 240
+        logo.drawHeight = 200
+        logo.drawWidth = 200
         logo.hAlign = "CENTER"
         elements.append(logo)
     except:
