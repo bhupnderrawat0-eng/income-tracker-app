805705909c9550a4ad5a9e12ec9f52114bf40e8d
bhupnderrawat0-eng <bhupnderrawat0@gmail.com>
2026-06-24 20:13:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -383,6 +383,7 @@ def add_bg_from_local(image_file):
         """,
         unsafe_allow_html=True
     )
+add_bg_from_local("login_bg.png")
 # ================= LOGIN HEADER =================
 
 col1, col2, col3 = st.columns([1, 4, 1])
