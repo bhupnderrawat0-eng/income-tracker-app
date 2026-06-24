d424132ce1387518787b8d166db97a2386d1a6b4
bhupnderrawat0-eng <bhupnderrawat0@gmail.com>
2026-06-24 19:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ with col1:
         <div style="
             margin-top:70px;
             text-align:right;
-            margin-right:-230px;
+            margin-right:-300px;
         ">
         """,
         unsafe_allow_html=True
